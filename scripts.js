d23070529fd2b91473e5b37001529798ce262fb1
Moises Warlen <moisesfatecads@gmail.com>
2021-04-28 17:04:39 -0300

    segunda aula JS aprendendo sobre variaveis

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//let idade = 5;
+//console.log(idade);
+//let altura =180;
+//console.log(altura);
+let nome = 'Moises';
+let idade = 38;
+let sexo ='M';
+console.log("Nome: "+ nome + ", Idade: "+ idade  + " , Sexo: " + sexo);
